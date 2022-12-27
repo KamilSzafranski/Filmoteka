11741b06f050f1f39429d87f1c352b2633b9f7d5
Kamil Szafrański <107311803+KamilSzafranski@users.noreply.github.com>
2022-12-27 21:28:31 +0100

    update

---
+++ src/JS/main.js
@@ -0,0 +1,11 @@
+const GALLERY = document.querySelector("ul.MainPage__Grid");
+const GALLERY_TEMPLATE = document.querySelector("template.GalleryTemplate");
+const NUMBEF_OF_PHOTO = 20;
+
+const createTemplateGallery = number => {
+  for (let i = 0; i < number; i++) {
+    GALLERY.append(GALLERY_TEMPLATE.content.cloneNode(true));
+  }
+};
+
+createTemplateGallery(2);
