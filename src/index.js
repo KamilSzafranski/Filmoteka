8f37516bbd841ed1f177d725d7d56faa75a8f19f
Piotr Formicki <piotr.formicki18@gmail.com>
2022-12-28 19:07:07 +0100

    Merge branch 'dev' of https://github.com/KamilSzafranski/piatkowyDeploy into dev

--- src/index.js
+++ src/index.js
@@ -1,6 +1,21 @@
+<<<<<<< Updated upstream
 
 import { getMovies } from "./JS/api";
 import { createTemplateGallery } from "./JS/main";
 
 createTemplateGallery(20);
 getMovies()
+=======
+<<<<<<< HEAD
+import { createTemplateGallery } from "./JS/main";
+
+createTemplateGallery(20);
+=======
+
+import { getMovies } from "./JS/api";
+import { createTemplateGallery } from "./JS/main";
+
+createTemplateGallery(20);
+getMovies()
+>>>>>>> ded1dcc3cbc29b49e4e0ed010df25365e289374d
+>>>>>>> Stashed changes
