8f70fdcdbfb49519a155e7289df6a7b195affb28
Kamil Szafrański <107311803+KamilSzafranski@users.noreply.github.com>
2022-12-29 16:14:05 +0100

    update

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import { getMovies } from "./JS/api";
 import { createTemplateGallery } from "./JS/main";
+import "./JS/darkmode";
+import "./JS/library";
 createTemplateGallery(20);
 getMovies();
-
-import './JS/darkmode'
