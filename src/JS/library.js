69e9b5b68e61cf71522150cf1bdf2eb95c3e3233
Piotr Formicki <piotr.formicki18@gmail.com>
2022-12-29 13:49:27 +0100

    Stylowanie headera
    1) Przepuszczony kod scss przez autoprefixer
    2) Pracuje nad tym zeby walidator nie zwracal bledu przy wielu potomkach w tagu <label>

---
+++ src/JS/library.js
@@ -0,0 +1,16 @@
+const headerSearch = document.querySelector(".js-box");
+const library = document.querySelector("#library");
+const home = document.querySelector("#home");
+const header = document.querySelector(".header");
+
+const libraryCreation = e => {
+  e.preventDefault();
+  headerSearch.innerHTML = "";
+  headerSearch.innerHTML = `<div class="js-box--padding"><button class="btn-watched btn-active" type="button">Watched</button>
+        <button class="btn-queue" type="button">Queue</button></div>`;
+  library.classList.add("nav-list__link--active");
+  home.classList.remove("nav-list__link--active");
+  header.classList.add("header--bgc");
+};
+
+library.addEventListener("click", libraryCreation);
