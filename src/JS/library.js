1332fe09777fa1095e26bb155074ee172be02d9c
Piotr Formicki <piotr.formicki18@gmail.com>
2023-01-06 09:14:10 +0100

    dne

--- src/JS/library.js
+++ src/JS/library.js
@@ -7,6 +7,7 @@ import Notiflix from "notiflix";
 Notiflix.Notify.init({
   position: "center-top",
   clickToClose: true,
+  background: "#ff6b08",
 });
 const libraryCreation = e => {
   e.preventDefault();
