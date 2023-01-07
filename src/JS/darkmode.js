17ad88f12385af4d2154cd6922e3faa30548f789
AniaPiWo <snowflake@wp.pl>
2023-01-07 13:56:43 +0100

    darkmode
    - fil modal
    -paginacja

--- src/JS/darkmode.js
+++ src/JS/darkmode.js
@@ -1,17 +1,24 @@
 const BODY = document.querySelector("body");
 const FOOTER = document.querySelector(".footer");
 const TOGGLE = document.querySelector(".darkmode-toggle");
+const MODAL = document.querySelector(".film-modal")
+const PAGINATION = document.querySelector(".Pagination")
 
 let getMode = localStorage.getItem("mode");
+
 if (getMode && getMode === "dark") {
   BODY.classList.add("dark");
   FOOTER.classList.add("dark");
+  MODAL.classList.add("dark");
+  PAGINATION.classList.add("dark");
   TOGGLE.classList.add("active");
 }
 
 TOGGLE.addEventListener("click", () => {
   BODY.classList.toggle("dark");
   FOOTER.classList.toggle("dark");
+  MODAL.classList.add("dark");
+  PAGINATION.classList.add("dark");
 
   if (!BODY.classList.contains("dark")) {
     return localStorage.setItem("mode", "light");
